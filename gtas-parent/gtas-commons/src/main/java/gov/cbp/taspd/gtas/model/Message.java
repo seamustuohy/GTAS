275678f9f7d8343b8431700bfb5a717cd02b54e1
Amit <amit_bandopadhay@yahoo.com>
2015-06-10 11:38:16 -0500

    Temporary fix for errors due to migration from Hibernate 3 -> 4

--- gtas-parent/gtas-commons/src/main/java/gov/cbp/taspd/gtas/model/Message.java
+++ gtas-parent/gtas-commons/src/main/java/gov/cbp/taspd/gtas/model/Message.java
@@ -2,14 +2,29 @@ package gov.cbp.taspd.gtas.model;
 
 import java.util.Date;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Lob;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)  
-public class Message extends BaseEntity {
+public class Message  {
+    @Id  
+//    @GeneratedValue(strategy = GenerationType.AUTO)  
+//    @Basic(optional = false)  
+//    @Column(name = "id", nullable = false, columnDefinition = "BIGINT UNSIGNED")  
+    protected Long id;  
+  
+    public Long getId() {  
+        return id;  
+    }  
+    
     private String transmissionSource;
     
     @Lob
