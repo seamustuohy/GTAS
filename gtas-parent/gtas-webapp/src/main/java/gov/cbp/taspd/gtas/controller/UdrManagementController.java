dc904bc9c1e7fdadcb62e41f77fec736d7373d41
Amit <amit_bandopadhay@yahoo.com>
2015-06-11 19:28:15 -0500

    fixed typo

--- gtas-parent/gtas-webapp/src/main/java/gov/cbp/taspd/gtas/controller/UdrManagementController.java
+++ gtas-parent/gtas-webapp/src/main/java/gov/cbp/taspd/gtas/controller/UdrManagementController.java
@@ -2,13 +2,7 @@ package gov.cbp.taspd.gtas.controller;
 
 import gov.cbp.taspd.gtas.bo.RuleExecutionStatistics;
 import gov.cbp.taspd.gtas.constants.URIConstants;
-import gov.cbp.taspd.gtas.querybuilder.QueryBuilderService;
 import gov.cbp.taspd.gtas.svc.TargetingService;
-import gov.cbp.taspd.gtas.web.model.QueryBuilderTable;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
