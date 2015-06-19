f65a0de70575d0bbcd55929e9814586be75a7946
Amit <amit_bandopadhay@yahoo.com>
2015-06-19 01:18:58 -0500

    deleted user repository and moved it to gtas commons.

--- gtas-parent/gtas-webapp/src/main/java/gov/gtas/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package gov.gtas.repository;
-
-import gov.gtas.model.User;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, String> {
-
-}
