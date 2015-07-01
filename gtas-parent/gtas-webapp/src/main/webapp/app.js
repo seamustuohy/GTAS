909eb6fb2bc74457ee2e022f62b599fcc118a576
copenhafer <mikecope@gmail.com>
2015-07-01 16:19:43 -0400

    initial tabs, flight ui, directory layout.

--- gtas-parent/gtas-webapp/src/main/webapp/app.js
+++ gtas-parent/gtas-webapp/src/main/webapp/app.js
@@ -1,39 +1,29 @@
-var app = angular.module('myApp', ['ui.router', 'ui.bootstrap', 'ngTable' ])
+var app = angular.module('myApp', [
+	'ui.router', 
+	'ui.bootstrap', 
+	'ngTable'
+]);
 
 app.config(function($stateProvider){
 $stateProvider
-    .state('index', {
-        url: '',
-        views: {
-            'main': {
-                template: 'index.main'
-            },
-            'viewB': {
-                template: 'index.viewB'
-            }
-        }
-    })
     .state('flights', {
         url: '/flights',
-        views: {
-            'main': {
-                templateUrl: 'views/flights.html',
-                controller: 'FlightController'
-            },
-            'viewB': {
-                template: 'flights.viewB'
-            }
-        }
+        templateUrl: 'flights/flights.html',
+        controller: 'FlightsController'
     })
     .state('query', {
         url: '/query',
-        views: {
-            'main': {
-                template: 'query.main'
-            },
-            'viewB': {
-                template: 'query.viewB'
-            }
-        }
+        templateUrl: 'query/query.html'
+    })
+    .state('admin', {
+        url: '/admin',
+        templateUrl: 'admin/admin.html'
     })
 })
+
+app.controller('TabsCtrl', function ($scope, $window, $state) {
+  $scope.gotoState = function(s) {
+  	$state.go(s);
+  };
+});
+
