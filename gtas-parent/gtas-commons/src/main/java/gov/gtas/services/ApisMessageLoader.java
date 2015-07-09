c519db51c5324b51d0a3296eac5847036ece9b5b
copenhafer <mikecope@gmail.com>
2015-07-08 21:10:34 -0400

    process both files and directories

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/services/ApisMessageLoader.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/services/ApisMessageLoader.java
@@ -1,43 +1,57 @@
 package gov.gtas.services;
 
-import java.text.ParseException;
-
 import gov.gtas.config.CommonServicesConfig;
 import gov.gtas.parsers.paxlst.vo.ApisMessageVo;
+
+import java.io.File;
+import java.text.ParseException;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApisMessageLoader {
+    public static void processSingleFile(ApisMessageService svc, String filePath) {
+        System.out.println("processing file " + filePath);
+        ApisMessageVo m = null;
+        try {
+            m = svc.parseApisMessage(filePath);
+        } catch (ParseException pe) {
+            pe.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (m == null) {
+            System.out.println("error parsing " + filePath);
+            return;
+        }
+
+        try {
+            svc.loadApisMessage(m);
+        } catch (Exception e) {
+            System.out.println("error loading " + filePath);
+        }
+    }
+
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Usage: ApisMessageLoader [apis files]");
+            System.out.println("Usage: ApisMessageLoader [apis files or folder name]");
             System.exit(0);
         }
-        
+
         try (ConfigurableApplicationContext ctx = new AnnotationConfigApplicationContext(CommonServicesConfig.class)) {
             ApisMessageService svc = ctx.getBean(ApisMessageService.class);
-            for (int i=0; i<args.length; i++) {
-                String filePath = args[i];
-                System.out.println("processing file " + filePath);
-                ApisMessageVo m = null;
-                try {
-                    m = svc.parseApisMessage(filePath);
-                } catch (ParseException pe) {
-                    pe.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                
-                if (m == null) {
-                    System.out.println("error parsing " + filePath);
-                    continue;
-                }
-                
-                try {
-                    svc.loadApisMessage(m);
-                } catch (Exception e) {
-                    System.out.println("error loading " + filePath);
+            for (int i = 0; i < args.length; i++) {
+                File tmp = new File(args[i]);
+                if (tmp.isFile()) {
+                    processSingleFile(svc, args[i]);
+                } else if (tmp.isDirectory()) {
+                    File[] listOfFiles = tmp.listFiles();
+                    for (int j = 0; j < listOfFiles.length; j++) {
+                        if (listOfFiles[j].isFile()) {
+                            processSingleFile(svc, listOfFiles[j].getAbsolutePath());
+                        }
+                    }
                 }
             }
         }
