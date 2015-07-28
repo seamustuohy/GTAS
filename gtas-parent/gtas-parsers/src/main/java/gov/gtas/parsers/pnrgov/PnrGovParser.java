bf4a8a2cb53b73dd75d789ff5071d5885b14acd9
Michael Copenhafer <mcopenhafer@rockcreek.lan>
2015-07-28 14:35:44 -0400

    org pnr segment parsing rules added.

--- gtas-parent/gtas-parsers/src/main/java/gov/gtas/parsers/pnrgov/PnrGovParser.java
+++ gtas-parent/gtas-parsers/src/main/java/gov/gtas/parsers/pnrgov/PnrGovParser.java
@@ -7,21 +7,25 @@ import java.util.Set;
 import gov.gtas.parsers.edifact.EdifactLexer;
 import gov.gtas.parsers.edifact.EdifactParser;
 import gov.gtas.parsers.exception.ParseException;
+import gov.gtas.parsers.pnrgov.segment.ADD;
 import gov.gtas.parsers.pnrgov.segment.DAT_G1;
+import gov.gtas.parsers.pnrgov.segment.EBD;
 import gov.gtas.parsers.pnrgov.segment.EQN_L0;
 import gov.gtas.parsers.pnrgov.segment.IFT;
 import gov.gtas.parsers.pnrgov.segment.MSG;
+import gov.gtas.parsers.pnrgov.segment.ORG_G1;
 import gov.gtas.parsers.pnrgov.segment.ORG_L0;
 import gov.gtas.parsers.pnrgov.segment.RCI;
 import gov.gtas.parsers.pnrgov.segment.SRC;
 import gov.gtas.parsers.pnrgov.segment.SSR_G1;
+import gov.gtas.parsers.pnrgov.segment.TIF;
 import gov.gtas.parsers.pnrgov.segment.TVL_L0;
 import gov.gtas.parsers.pnrgov.vo.PnrMessageVo;
 
 public final class PnrGovParser extends EdifactParser<PnrMessageVo> {
-    private static final String[] SEGMENT_NAMES = new String[] { "ABI", "APD", "DAT", "EBD", "EQN", "FAR", "FOP", "FTI",
-            "IFT", "LTS", "MON", "MSG", "ORG", "PTK", "RCI", "REF", "RPI", "SAC", "SRC", "SSD", "SSR", "TBD", "TIF",
-            "TKT", "TRA", "TRI", "TVL", "TXD" };
+    private static final String[] SEGMENT_NAMES = new String[] { "ABI", "ADD", "APD", "DAT", "EBD", "EQN", "FAR", "FOP",
+            "FTI", "IFT", "LTS", "MON", "MSG", "ORG", "PTK", "RCI", "REF", "RPI", "SAC", "SRC", "SSD", "SSR", "TBD",
+            "TIF", "TKT", "TRA", "TRI", "TVL", "TXD" };
     public static final Set<String> PNRGOV_SEGMENT_INDEX = new HashSet<>(Arrays.asList(SEGMENT_NAMES));
     
     public PnrGovParser() {
@@ -61,7 +65,15 @@ public final class PnrGovParser extends EdifactParser<PnrMessageVo> {
             processGroup1();
         }
 
-
+        TIF tif = getMandatorySegment(TIF.class);
+        processGroup2(tif);
+        for (;;) {
+            tif = getConditionalSegment(TIF.class);
+            if (tif == null) {
+                break;
+            }
+            processGroup2(tif);
+        }
     }
     
     public void processGroup1() throws ParseException {
@@ -86,5 +98,26 @@ public final class PnrGovParser extends EdifactParser<PnrMessageVo> {
             System.out.println(ift);
         }
 
+        ORG_G1 org = getMandatorySegment(ORG_G1.class);
+
+        for (;;) {
+            ADD add = getConditionalSegment(ADD.class);
+            if (add == null) {
+                break;
+            }
+            System.out.println(add);
+        }
+
+        for (;;) {
+            EBD ebd = getConditionalSegment(EBD.class);
+            if (ebd == null) {
+                break;
+            }
+            System.out.println(ebd);
+        }
+    }
+
+    public void processGroup2(TIF tif) throws ParseException {
+        System.out.println(tif);
     }
 }
