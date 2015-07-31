6596f5660374c507f5c4f35203e90765c34bc9ea
Michael Copenhafer <mcopenhafer@rockcreek.home>
2015-07-31 15:45:02 -0400

    updated tvl segment

--- gtas-parent/gtas-parsers/src/main/java/gov/gtas/parsers/pnrgov/segment/TVL.java
+++ gtas-parent/gtas-parsers/src/main/java/gov/gtas/parsers/pnrgov/segment/TVL.java
@@ -33,9 +33,6 @@ import gov.gtas.parsers.pnrgov.PnrUtils;
  * city/airport codes as contained in the passenger’s booked itinerary.
  */
 public class TVL extends Segment {
-    private String groupName;
-    private String level;
-    
     private Date etd;
     private Date eta;
     private String origin;
@@ -44,17 +41,6 @@ public class TVL extends Segment {
     private String operatingCarrier;
     private String flightNumber;
     private String reservationBookingDesignator;
-
-    private String dateVariation;
-    private String lastIataCode;
-    private String lastIataName;
-
-    private String operationFlightSuffix;
-    private String placeOfDeparture;
-    private String placeOfArrival;
-    private String airlineCode;
-    private String serviceProductCode;
-    private String serviceIdentificationCode;
 
     public TVL(List<Composite> composites) throws ParseException {
         super(TVL.class.getSimpleName(), composites);
@@ -78,7 +64,8 @@ public class TVL extends Segment {
                 this.operatingCarrier = c.getElement(1);
                 break;
             case 4:
-                
+                this.flightNumber = c.getElement(0);
+                this.reservationBookingDesignator = c.getElement(1);
             }                
         }
     }
@@ -110,5 +97,37 @@ public class TVL extends Segment {
         }
         
         return rv;
+    }
+
+    public Date getEtd() {
+        return etd;
+    }
+
+    public Date getEta() {
+        return eta;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public String getOperatingCarrier() {
+        return operatingCarrier;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getReservationBookingDesignator() {
+        return reservationBookingDesignator;
     }
 }
