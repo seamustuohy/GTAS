9d53c45a8a600b52b9efef4ee93012f8b2d80874
ladebiyi <ladebiyi@yahoo.com>
2015-07-31 11:06:13 -0400

    Updated Unit Test, using mock framework

--- gtas-parent/gtas-query-builder/src/test/java/gov/gtas/querybuilder/service/QueryBuilderServiceTest.java
+++ gtas-parent/gtas-query-builder/src/test/java/gov/gtas/querybuilder/service/QueryBuilderServiceTest.java
@@ -1,248 +1,80 @@
-/**
- * 
- */
 package gov.gtas.querybuilder.service;
 
-import static org.junit.Assert.assertEquals;
-import gov.gtas.model.User;
-import gov.gtas.model.udr.json.QueryEntity;
-import gov.gtas.model.udr.json.QueryObject;
-import gov.gtas.model.udr.json.QueryTerm;
-import gov.gtas.querybuilder.constants.Constants;
-import gov.gtas.querybuilder.enums.EntityEnum;
-import gov.gtas.querybuilder.model.UserQuery;
+import static org.junit.Assert.*;
+import gov.gtas.querybuilder.repository.QueryBuilderRepository;
 
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-/**
- * @author GTAS5
- *
- */
-@Ignore
- 
 public class QueryBuilderServiceTest {
 
-	QueryBuilderService service;
-	 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	@Mock
+	QueryBuilderRepository queryRepository;
+	@InjectMocks
+	QueryBuilderService queryService;
+	
 	@Before
-	public void setUp() throws Exception {
-		service = new QueryBuilderService();
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+	@Test
+	public void testSaveQuery() {
+//		Mockito.when(queryService.saveQuery(null)).
 	}
 
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@After
-	public void tearDown() throws Exception {
-		service = null;
+	@Test
+	public void testSaveDuplicateQuery() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSaveQueryWithNullDescription() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSaveInvalidQuery() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEditQuery() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testEditQueryToDuplicateTitle() {
+		fail("Not yet implemented");
 	}
 
-	
-//	@Test()
-	public void test() {
-		UserQuery query = new UserQuery();
-		User user = new User();
-		user.setUserId("ladebiyi");
-		
-		query.setCreatedBy(user);
-		query.setCreatedDt(new Date());
-		query.setTitle("Test Query");
-		query.setDescription("Test description");
-//		query.setQuery(buildSimpleQuery());
-		
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			System.out.println("Query: " + mapper.writerWithDefaultPrettyPrinter().writeValueAsString(query));
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	@Test
+	public void testListQueryByUser() {
+		fail("Not yet implemented");
 	}
 	
-	@Test()
-	public void testGetQueryForFlightsWithSimpleQuery() throws Exception {
-		final String expectedQuery = "";
-//		final String expectedQuery = Constants.SELECT_DISTINCT + " " + EntityEnum.FLIGHT.getAlias() + 
-//				" " + Constants.FROM + " " + EntityEnum.FLIGHT.getEntityName() + " " + EntityEnum.FLIGHT.getAlias() +
-//				QueryBuilderUtil.getJoinCondition(EntityEnum.PAX) + " " + Constants.WHERE + " p.firstName = 'DAVID'";
-		
-		QueryObject query = buildSimpleQuery();
-		
-		Method privateGetQueryMethod = QueryBuilderService.class.getDeclaredMethod("getQuery", QueryObject.class, EntityEnum.class);
-		privateGetQueryMethod.setAccessible(true);
+	@Test
+	public void testListQueryByInvalidUser() {
+		fail("Not yet implemented");
+	}
 
-		String actualQuery = (String) privateGetQueryMethod.invoke(service, query, EntityEnum.FLIGHT);
-		System.out.println("actualQuery: " + actualQuery);
-//		assertEquals(expectedQuery, actualQuery);
+	@Test
+	public void testDeleteQuery() {
+		fail("Not yet implemented");
 	}
-	
-//	@Test()
-	public void testGetQueryForPassengersWithSimpleQuery() throws Exception {
-		final String expectedQuery = Constants.SELECT_DISTINCT + " " + EntityEnum.TRAVELER.getAlias() + 
-				" " + Constants.FROM + " " + EntityEnum.TRAVELER.getEntityName() + " " + EntityEnum.TRAVELER.getAlias() +
-				" " + Constants.WHERE + " p.firstName = 'DAVID'";
-		
-		QueryObject query = buildSimpleQuery();
-		
-		Method privateGetQueryMethod = QueryBuilderService.class.getDeclaredMethod("getQuery", QueryObject.class, EntityEnum.class);
-		privateGetQueryMethod.setAccessible(true);
-		
-		String actualQuery = (String) privateGetQueryMethod.invoke(service, query, EntityEnum.TRAVELER);
-		
-		assertEquals(expectedQuery, actualQuery);
-		
-	}
-	
-//	@Test()
-	public void testGetQueryForFlightsWithSimpleDateQuery() throws Exception {
-		final String expectedQuery = "";
-//		final String expectedQuery = Constants.SELECT_DISTINCT + " " + EntityEnum.FLIGHT.getAlias() + 
-//				" " + Constants.FROM + " " + EntityEnum.FLIGHT.getEntityName() + " " + EntityEnum.FLIGHT.getAlias() +
-//				QueryBuilderUtil.getJoinCondition(EntityEnum.PAX) + " " + Constants.WHERE + " p.firstName = 'DAVID'";
-		
-		QueryObject query = buildSimpleDateQuery();
-		
-		Method privateGetQueryMethod = QueryBuilderService.class.getDeclaredMethod("getQuery", QueryObject.class, EntityEnum.class);
-		privateGetQueryMethod.setAccessible(true);
 
-		String actualQuery = (String) privateGetQueryMethod.invoke(service, query, EntityEnum.FLIGHT);
-		
-		System.out.println("actual Query: " + actualQuery);
-		
-//		assertEquals(expectedQuery, actualQuery);
+	@Test
+	public void testRunFlightQuery() {
+		fail("Not yet implemented");
 	}
-	
-//	@Test()
-	public void testGetQueryForFlightsWithSimpleIsNullQuery() throws Exception {
-		final String expectedQuery = "";
-//		final String expectedQuery = Constants.SELECT_DISTINCT + " " + EntityEnum.FLIGHT.getAlias() + 
-//				" " + Constants.FROM + " " + EntityEnum.FLIGHT.getEntityName() + " " + EntityEnum.FLIGHT.getAlias() +
-//				QueryBuilderUtil.getJoinCondition(EntityEnum.PAX) + " " + Constants.WHERE + " p.firstName = 'DAVID'";
-		
-		QueryObject query = buildSimpleIsNullQuery();
-		
-		Method privateGetQueryMethod = QueryBuilderService.class.getDeclaredMethod("getQuery", QueryObject.class, EntityEnum.class);
-		privateGetQueryMethod.setAccessible(true);
 
-		String actualQuery = (String) privateGetQueryMethod.invoke(service, query, EntityEnum.FLIGHT);
-		
-		System.out.println("actual Query: " + actualQuery);
-		
-//		assertEquals(expectedQuery, actualQuery);
+	@Test
+	public void testRunPassengerQuery() {
+		fail("Not yet implemented");
 	}
-	
-//	@Test()
-	public void testGetQueryForFlightsWithSimpleContainsQuery() throws Exception {
-		final String expectedQuery = "";
-//		final String expectedQuery = Constants.SELECT_DISTINCT + " " + EntityEnum.FLIGHT.getAlias() + 
-//				" " + Constants.FROM + " " + EntityEnum.FLIGHT.getEntityName() + " " + EntityEnum.FLIGHT.getAlias() +
-//				QueryBuilderUtil.getJoinCondition(EntityEnum.PAX) + " " + Constants.WHERE + " p.firstName = 'DAVID'";
-		
-		QueryObject query = buildSimpleContainsQuery();
-		
-		Method privateGetQueryMethod = QueryBuilderService.class.getDeclaredMethod("getQuery", QueryObject.class, EntityEnum.class);
-		privateGetQueryMethod.setAccessible(true);
 
-		String actualQuery = (String) privateGetQueryMethod.invoke(service, query, EntityEnum.FLIGHT);
-		
-		System.out.println("actual Query: " + actualQuery);
-		
-//		assertEquals(expectedQuery, actualQuery);
-	}
-	
-	private QueryObject buildSimpleQuery() {
-		QueryObject query = new QueryObject();
-		QueryTerm rule = new QueryTerm();
-		List<QueryEntity> rules = new ArrayList<>();
-		
-		rule.setEntity("Pax");
-		rule.setField("firstName");
-		rule.setOperator("equal");
-		rule.setType("string");
-		rule.setValue("DAVID");
-		
-		rules.add(rule);
-		
-		query.setCondition("AND");
-		query.setRules(rules);
-		
-		return query;
-	}
-	
-	private QueryObject buildSimpleDateQuery() {
-		QueryObject query = new QueryObject();
-		QueryTerm rule = new QueryTerm();
-		List<QueryEntity> rules = new ArrayList<>();
-		
-		rule.setEntity("Flight");
-		rule.setField("eta");
-		rule.setOperator("equal");
-		rule.setType("date");
-		rule.setValue("05/11/2014");
-		
-		rules.add(rule);
-		
-		query.setCondition("AND");
-		query.setRules(rules);
-		
-		return query;
-	}
-	
-	private QueryObject buildSimpleIsNullQuery() {
-		QueryObject query = new QueryObject();
-		QueryTerm rule = new QueryTerm();
-		List<QueryEntity> rules = new ArrayList<>();
-		
-		rule.setEntity("Pax");
-		rule.setField("middleName");
-		rule.setOperator("is_null");
-		rule.setType("boolean");
-		rule.setValue("");
-		
-		rules.add(rule);
-		
-		query.setCondition("AND");
-		query.setRules(rules);
-		
-		return query;
-	}
-	
-	private QueryObject buildSimpleContainsQuery() {
-		QueryObject query = new QueryObject();
-		QueryTerm rule = new QueryTerm();
-		List<QueryEntity> rules = new ArrayList<>();
-		
-		rule.setEntity("Pax");
-		rule.setField("firstName");
-		rule.setOperator("contains");
-		rule.setType("string");
-		rule.setValue("avi");
-		
-		rules.add(rule);
-		
-		query.setCondition("AND");
-		query.setRules(rules);
-		
-		return query;
-	}
-	
-	private QueryObject buildNestedQuery() {
-		QueryObject query = new QueryObject();
-		
-		return query;
-	}
 }
