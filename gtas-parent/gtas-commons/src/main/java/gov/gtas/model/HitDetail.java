11aab2e9e97a96d80bbe32950985cb424d6684ff
jtangGitHub <jtangGitHub@gmail.com>
2015-08-03 13:06:39 -0400

    add HitDetail and update HitsSummary

---
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/model/HitDetail.java
@@ -0,0 +1,65 @@
+package gov.gtas.model;
+
+import java.util.Date;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "hit_detail")
+public class HitDetail extends BaseEntity {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5219262569468670275L;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "hits_summary_id", nullable = false, referencedColumnName = "id")
+	private HitsSummary parent;
+
+	@Lob
+	@Basic(fetch = FetchType.EAGER)
+	@Column(name = "cond_blob", columnDefinition = "BLOB NULL")
+	private String[] ruleConditions;
+
+	@Column(name = "create_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date createDate;
+
+	@Column(name = "rule_id", nullable = false)
+	private Long ruleId;
+
+	public HitsSummary getParent() {
+		return parent;
+	}
+
+	public void setParent(HitsSummary parent) {
+		this.parent = parent;
+	}
+
+	public String[] getRuleConditions() {
+		return ruleConditions;
+	}
+
+	public void setRuleConditions(String[] ruleConditions) {
+		this.ruleConditions = ruleConditions;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+}
