245a55c91f45ebdf5272759d2fbb42ddd244aee1
Anthony Delorie <de.lorie@gmail.com>
2015-08-28 01:37:43 -0400

    added movable attribute to flights and modal-passenger grid, and query results grid (either pass or flights)

--- gtas-parent/gtas-webapp/src/main/webapp/factory/ModalGridFactory.js
+++ gtas-parent/gtas-webapp/src/main/webapp/factory/ModalGridFactory.js
@@ -4,7 +4,7 @@ app.factory('modal', ['$compile', '$rootScope', function ($compile, $rootScope) 
         var elm,
             modal = {
                 open: function () {
-                    var html = '<div class="modal" ng-style="modalStyle">{{modalStyle}}<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 style="font-size: 18px;" align="left" class="ng-binding"><span class="glyphicon glyphicon-user glyphiconFlightPax img-circle"></span> Passengers on Flight - {{selectedFlightNumber}}</h4></div><div class="modal-body"><div id="grid1" ui-grid="gridOptions" ui-grid-pagination ui-grid-exporter ui-grid-resize-columns class="grid"></div></div><div class="modal-footer"><button id="buttonClose" class="btn btn-primary" ng-click="close()">Close</button></div></div></div></div>';
+                    var html = '<div class="modal" ng-style="modalStyle">{{modalStyle}}<div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 style="font-size: 18px;" align="left" class="ng-binding"><span class="glyphicon glyphicon-user glyphiconFlightPax img-circle"></span> Passengers on Flight - {{selectedFlightNumber}}</h4></div><div class="modal-body"><div id="grid1" ui-grid="gridOptions" ui-grid-pagination ui-grid-exporter ui-grid-resize-columns ui-grid-move-columns class="grid"></div></div><div class="modal-footer"><button id="buttonClose" class="btn btn-primary" ng-click="close()">Close</button></div></div></div></div>';
                     elm = angular.element(html);
                     angular.element(document.body).prepend(elm);
 
