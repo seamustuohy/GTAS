29baf6e456496a517bb8db8fd58434d1285a577f
Srinivasarao <vempati.svr@gmail.com>
2015-08-28 11:00:56 -0400

    Added missing files to vo folder

---
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/delegates/vo/PnrReportingAgentVo.java
@@ -0,0 +1,61 @@
+package gov.gtas.delegates.vo;
+
+public class PnrReportingAgentVo {
+    private String airlineCode;
+    private String locationCode;
+    private String identificationNumber;
+    private String identificationCode;
+    private String locationIdentificationCode;
+    private String countryCode;
+    private String currencyCode;
+    private String localeCode;
+    
+    public String getAirlineCode() {
+        return airlineCode;
+    }
+    public void setAirlineCode(String airlineCode) {
+        this.airlineCode = airlineCode;
+    }
+    public String getLocationCode() {
+        return locationCode;
+    }
+    public void setLocationCode(String locationCode) {
+        this.locationCode = locationCode;
+    }
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+    public String getIdentificationCode() {
+        return identificationCode;
+    }
+    public void setIdentificationCode(String identificationCode) {
+        this.identificationCode = identificationCode;
+    }
+    public String getLocationIdentificationCode() {
+        return locationIdentificationCode;
+    }
+    public void setLocationIdentificationCode(String locationIdentificationCode) {
+        this.locationIdentificationCode = locationIdentificationCode;
+    }
+    public String getCountryCode() {
+        return countryCode;
+    }
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+    public String getCurrencyCode() {
+        return currencyCode;
+    }
+    public void setCurrencyCode(String currencyCode) {
+        this.currencyCode = currencyCode;
+    }
+    public String getLocaleCode() {
+        return localeCode;
+    }
+    public void setLocaleCode(String localeCode) {
+        this.localeCode = localeCode;
+    }
+}
