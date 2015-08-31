33452ea3e99db78505a26e6ef373c89b0f0367be
Amit <amit_bandopadhay@yahoo.com>
2015-08-31 10:55:28 -0400

    added missing data gen

---
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/util/SampleDataGenerator.java
@@ -0,0 +1,56 @@
+package gov.gtas.util;
+
+import gov.gtas.enumtype.EntityEnum;
+import gov.gtas.enumtype.WatchlistEditEnum;
+import gov.gtas.model.watchlist.json.WatchlistItemSpec;
+import gov.gtas.model.watchlist.json.WatchlistSpec;
+import gov.gtas.model.watchlist.json.WatchlistTerm;
+import gov.gtas.querybuilder.mappings.PassengerMapping;
+
+/**
+ * A builder pattern object for creating watch list objects programmatically.
+ * 
+ * @author GTAS3
+ *
+ */
+public class SampleDataGenerator {
+	/**
+	 * Creates a sample watch list JSON object. (This is used for testing.)
+	 * 
+	 * @return watch list JSON object.
+	 */
+	public static WatchlistSpec createSampleWatchlist(String wlName) {
+		WatchlistSpec ret = new WatchlistSpec(wlName,
+				EntityEnum.PASSENGER.getEntityName().toUpperCase());
+		ret.addWatchlistItem(new WatchlistItemSpec(null, WatchlistEditEnum.C
+				.getOperationName(), new WatchlistTerm[] {
+				new WatchlistTerm(EntityEnum.PASSENGER.getEntityName()
+						.toUpperCase(), PassengerMapping.FIRST_NAME
+						.getFieldName(), PassengerMapping.FIRST_NAME
+						.getFieldType(), "John"),
+				new WatchlistTerm(EntityEnum.PASSENGER.getEntityName()
+						.toUpperCase(), PassengerMapping.LAST_NAME
+						.getFieldName(), PassengerMapping.LAST_NAME
+						.getFieldType(), "Jones"),
+				new WatchlistTerm(EntityEnum.PASSENGER.getEntityName()
+						.toUpperCase(), PassengerMapping.DOB.getFieldName(),
+						PassengerMapping.DOB.getFieldType(), "1747-07-06") }));
+		ret.addWatchlistItem(new WatchlistItemSpec(32L, WatchlistEditEnum.U
+				.getOperationName(), new WatchlistTerm[] {
+				new WatchlistTerm(EntityEnum.PASSENGER.getEntityName()
+						.toUpperCase(), PassengerMapping.FIRST_NAME
+						.getFieldName(), PassengerMapping.FIRST_NAME
+						.getFieldType(), "Julius"),
+				new WatchlistTerm(EntityEnum.PASSENGER.getEntityName()
+						.toUpperCase(), PassengerMapping.LAST_NAME
+						.getFieldName(), PassengerMapping.LAST_NAME
+						.getFieldType(), "Seizure"),
+				new WatchlistTerm(EntityEnum.PASSENGER.getEntityName()
+						.toUpperCase(), PassengerMapping.DOB.getFieldName(),
+						PassengerMapping.DOB.getFieldType(), "1966-09-13") }));
+		ret.addWatchlistItem(new WatchlistItemSpec(25L, WatchlistEditEnum.D
+				.getOperationName(), null));
+		return ret;
+	}
+
+}
