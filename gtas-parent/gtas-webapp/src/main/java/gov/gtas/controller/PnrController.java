5edf17a66065a750a465b2de8eba0f8028f04b50
Michael Copenhafer <mcopenhafer@rockcreek.local>
2015-09-02 13:38:17 -0400

    added preliminary services for retrieving PNR data given pnr id. also method for retrieving record locators for a given passenger id.

---
+++ gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/PnrController.java
@@ -0,0 +1,39 @@
+package gov.gtas.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import gov.gtas.delegates.PnrServiceDelegate;
+import gov.gtas.delegates.vo.PnrDataVo;
+
+@Controller
+public class PnrController {
+    private static final Logger logger = LoggerFactory.getLogger(PnrController.class);
+    
+    @Autowired
+    private PnrServiceDelegate pnrDelegate;
+    
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/pnrdata/{pnrId}", method = RequestMethod.GET)
+    public PnrDataVo getPnrData(@PathVariable Long pnrId) {
+        return pnrDelegate.getAllPnrData(pnrId);
+    }    
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(value = "/pnrdata/pax/{paxId}", method = RequestMethod.GET)
+    public List<PnrDataVo> getRecordLocators(@PathVariable Long paxId) {
+        return pnrDelegate.getRecordLocators(paxId);
+    }    
+}
