7ead9119fba6f700b83748ff7aedfa4569bfe253
Anthony Delorie <de.lorie@gmail.com>
2015-09-03 14:18:14 -0400

    Watchlist now functional with REST services. had to change property params: -> data:

--- gtas-parent/gtas-webapp/src/main/webapp/watchlists/WatchListService.js
+++ gtas-parent/gtas-webapp/src/main/webapp/watchlists/WatchListService.js
@@ -10,23 +10,6 @@ app.service("watchListService", function ($http, $q) {
         handleSuccess = function (response) {
             return (response);
         },
-        getRequest = function (method, listTypeName, entity, id, terms) {
-            var action = !id ? 'Create' : !terms ? 'Delete' : 'Update';
-            return $http({
-                method: method,
-                url: baseUrl + 'adelorie', //to get once angular security in place
-                params: {
-                    "@class": "gov.gtas.model.watchlist.json.WatchlistSpec",
-                    "name": listTypeName,
-                    "entity": entity,
-                    "watchlistItems": [{
-                        "id": id,
-                        "action": action,
-                        "terms": terms
-                    }]
-                }
-            });
-        },
         service = {
             getTabs: function () {
                 // HAVE TO USE .json for now...
@@ -48,26 +31,26 @@ app.service("watchListService", function ($http, $q) {
 
                 return (request.then(handleSuccess, handleError));
             },
-            deleteItem: function (listTypeName, entity, id) {
-                var request;
-                var action = !id ? 'Create' : !terms ? 'Delete' : 'Update';
+            deleteItem: function (listTypeName, entity, id, terms) {
+                var request,
+                    url = baseUrl + 'adelorie',
+                    action = !id ? 'Create' : !terms ? 'Delete' : 'Update';
 
                 if (!listTypeName || !entity || !id) {
                     return false;
                 }
 
-                //Method put not delete until told otherwise
-//                request = getRequest('put', listTypeName, entity, id, null);
-                request = $http.put({
-                    url: baseUrl + 'adelorie', //to get once angular security in place
-                    params: {
+                request = $http({
+                    method: 'put',
+                    url: url,
+                    data: {
                         "@class": "gov.gtas.model.watchlist.json.WatchlistSpec",
                         "name": listTypeName,
                         "entity": entity,
                         "watchlistItems": [{
                             "id": id,
                             "action": action,
-                            "terms": []
+                            "terms": null
                         }]
                     }
                 });
@@ -75,19 +58,18 @@ app.service("watchListService", function ($http, $q) {
                 return (request.then(handleSuccess, handleError));
             },
             addItem: function (listTypeName, entity, id, terms) {
-                var request, action, url;
+                var request,
+                    url = baseUrl + 'adelorie',
+                    action = !id ? 'Create' : !terms ? 'Delete' : 'Update';
 
                 if (!listTypeName || !entity || !terms) {
                     return false;
                 }
-                //remove hardcoded adelorie once angular security in place
-                url = baseUrl + 'adelorie';
-                action = !id ? 'Create' : !terms ? 'Delete' : 'Update';
-//                request = getRequest('post', listTypeName, entity, id, terms);
+
                 request = $http({
-                    method: "post",
+                    method: 'post',
                     url: url,
-                    params: {
+                    data: {
                         "@class": "gov.gtas.model.watchlist.json.WatchlistSpec",
                         "name": listTypeName,
                         "entity": entity,
@@ -99,20 +81,21 @@ app.service("watchListService", function ($http, $q) {
                     }
                 });
 
-
                 return (request.then(handleSuccess, handleError));
             },
             updateItem: function (listTypeName, entity, id, terms) {
-                var request, action;
+                var request,
+                    url = baseUrl + 'adelorie',
+                    action = !id ? 'Create' : !terms ? 'Delete' : 'Update';
 
-                if (!listTypeName || !entity || !terms) {
+                if (!listTypeName || !entity || !id || !terms) {
                     return false;
                 }
 
-                action = !id ? 'Create' : !terms ? 'Delete' : 'Update';
-                request = $http.put({
-                    url: baseUrl + 'adelorie', //to get once angular security in place
-                    params: {
+                request = $http({
+                    method: 'put',
+                    url: url,
+                    data: {
                         "@class": "gov.gtas.model.watchlist.json.WatchlistSpec",
                         "name": listTypeName,
                         "entity": entity,
