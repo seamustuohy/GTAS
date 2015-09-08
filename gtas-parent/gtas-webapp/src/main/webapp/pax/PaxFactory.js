96c9a888d30c5a2e1c941a8bb3bb37be6f088830
Anthony Delorie <de.lorie@gmail.com>
2015-09-08 15:31:47 -0400

    simply jslinting, 'use strict'

--- gtas-parent/gtas-webapp/src/main/webapp/pax/PaxFactory.js
+++ gtas-parent/gtas-webapp/src/main/webapp/pax/PaxFactory.js
@@ -1,18 +1,18 @@
-app.factory("sharedPaxData", function() {
-	 var items = [];
-	    var itemsService = {};
-	    
-	    itemsService.add = function(item, index) {
-	        items[index] = item;
-	    };
-	    itemsService.list = function(index) {
-	        return items[index];
-	    };
-	    
-	    itemsService.getAll = function() {
-	        return items;
-	    };
-	    
-	    
-	    return itemsService;
+app.factory("sharedPaxData", function () {
+    'use strict';
+    var items = [], itemsService = {};
+
+    itemsService.add = function (item, index) {
+        items[index] = item;
+    };
+
+    itemsService.list = function (index) {
+        return items[index];
+    };
+
+    itemsService.getAll = function () {
+        return items;
+    };
+
+    return itemsService;
 });
