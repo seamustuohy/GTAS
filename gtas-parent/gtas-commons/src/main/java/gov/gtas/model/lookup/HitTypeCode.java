8c33faec5869e4390884d39b3225c658d22e4106
jtangGitHub <jtangGitHub@gmail.com>
2015-09-09 14:32:04 -0400

    added HitType code

---
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/model/lookup/HitTypeCode.java
@@ -0,0 +1,8 @@
+package gov.gtas.model.lookup;
+
+public enum HitTypeCode {
+    R,   // Rule Hit
+    P,   // Watchlist Passenger Hit
+    D,   // Watchlist Document Hit
+    PD 	 // Watchlist Passenger and Document Hit
+}
