223d4eb794e7312e9aac4cc065cbba290be5b295
jtangGitHub <jtangGitHub@gmail.com>
2015-09-09 20:01:46 -0400

    setup to retrieve external property values

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/config/CommonServicesConfig.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/config/CommonServicesConfig.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -88,6 +89,11 @@ public class CommonServicesConfig {
 
 	@Resource
 	private Environment env;
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 
 	@Bean
 	public DataSource dataSource() {
