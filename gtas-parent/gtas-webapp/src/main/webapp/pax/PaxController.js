35c84759e38f6262d9e6070669ca4d31ed7b1aa5
Anthony Delorie <de.lorie@gmail.com>
2015-09-22 11:40:27 -0400

    JSLINT | formatting only

--- gtas-parent/gtas-webapp/src/main/webapp/pax/PaxController.js
+++ gtas-parent/gtas-webapp/src/main/webapp/pax/PaxController.js
@@ -1,6 +1,6 @@
-app.controller('PaxController', function ($scope, $rootScope, $injector, GridControl, jQueryBuilderFactory, $filter, 
-											$q, paxService, sharedPaxData, riskCriteriaService, $stateParams, $state,
-                                            $timeout, $interval, uiGridConstants) {
+app.controller('PaxController', function ($scope, $rootScope, $injector, GridControl, jQueryBuilderFactory, $filter,
+                                          $q, paxService, sharedPaxData, riskCriteriaService, $stateParams, $state,
+                                          $timeout, $interval, uiGridConstants) {
     var self = this;
     $injector.invoke(jQueryBuilderFactory, this, {$scope: $scope});
     $injector.invoke(GridControl, this, {$scope: $scope});
@@ -15,10 +15,7 @@ app.controller('PaxController', function ($scope, $rootScope, $injector, GridCon
         return $scope.list(index);
     };
 
-      //--------Date functions-----------------------
-
-    $scope.dt;
-    $scope.dt3;
+    //--------Date functions-----------------------
     $scope.startDate = '';
     $scope.endDate = '';
     $scope.loading = true;
@@ -73,7 +70,6 @@ app.controller('PaxController', function ($scope, $rootScope, $injector, GridCon
             }, 0, 1);
             $scope.status.openeddt3 = false;
         }
-
     };
 
     $scope.dateOptions = {
@@ -120,7 +116,6 @@ app.controller('PaxController', function ($scope, $rootScope, $injector, GridCon
 
         return '';
     };
-
 
     $('#start-date').datepicker().on('changeDate', function (ev) {
         var element = angular.element($('#start-date'));
@@ -170,142 +165,127 @@ app.controller('PaxController', function ($scope, $rootScope, $injector, GridCon
         $endDate.val($scope.dt3.toString());
 
     }, 100);
-	//--------------------------------------------
-    
+    //--------------------------------------------
+
     $scope.passengerGrid = {
-            saveFocus: true,
-            saveScroll: true,
-            saveGroupingExpandedStates: true,
-    		minRowsToShow : 12,	
-            enableFiltering: true,
-            useExternalSorting: false,
-            paginationPageSizes: [],
-            paginationPageSize: 25,
-            useExternalPagination: true,
-            enableHorizontalScrollbar: true,
-            
-            keepLastSelected: true,
-            //useExternalSorting: true,
-            //pagingOptions: $scope.pagingOptions,        
-            filterOptions: $scope.filterOptions,
-            sortInfo: $scope.sortOptions,	
-            
-            columnDefs: [{"name": "ruleHits", "displayName": "H", width: 50, enableFiltering: false,
-			            	 "sort": {
-			                     direction: uiGridConstants.DESC,
-			                     priority: 0
-			                 }
-            			 },
-                         // {"name": "onWatchList", "displayName": "L", width: 50, enableFiltering: false},
-                         {"name": "lastName", "displayName": "Last Name", width: 175/*, enableFiltering: false*/,
-            			
-            				 "sort": {
-                                 direction: uiGridConstants.DESC,
-                                 priority: 1
-                             }
-                         },
-                         {"name": "firstName", "displayName": "First Name", width: 150/*, enableFiltering: false*/},
-                         {"name": "middleName", "displayName": "Middle", width: 100/*, enableFiltering: false*/},
-                         {"name": "flightNumber", "displayName": "Flight", width: 90/*, enableFiltering: false*/},
-                         {"name": "flightETA", "displayName": "ETA", width: 175, enableFiltering: false},
-                         {"name": "flightETD", "displayName": "ETD", width: 175, enableFiltering: false},
-                         {"name": "passengerType", "displayName": "Type", width: 50, enableFiltering: false},
-                         {"name": "gender", "displayName": "G", width: 50, enableFiltering: false},
-                         {"name": "dob", "displayName": "DOB", field: 'dob', cellFilter: 'date', width: 175, enableFiltering: false},
-                         {"name": "citizenshipCountry", "displayName": "CTZ", width: 75/*, enableFiltering: false*/},
-                         {"name": "passengerType", "displayName": "T", width: 100, enableFiltering: false},
-                         {"name": "documentType", "displayName": "T", width: 50, enableFiltering: false},
-                         {"name": "carrier", "displayName": "Carrier", width: 50/*, enableFiltering: false*/},
-                         {"name": "seat", "displayName": "Seat", width: 75, enableFiltering: false}] ,
+        saveFocus: true,
+        saveScroll: true,
+        saveGroupingExpandedStates: true,
+        minRowsToShow: 12,
+        enableFiltering: true,
+        useExternalSorting: false,
+        paginationPageSizes: [],
+        paginationPageSize: 25,
+        useExternalPagination: true,
+        enableHorizontalScrollbar: true,
 
-                        enableGridMenu: true,
-                 		enableSelectAll: false,
-                 		exporterPdfDefaultStyle: {fontSize: 9},
-                 		exporterPdfTableStyle: {margin: [10, 10, 10, 10]},
-                 		exporterPdfTableHeaderStyle: {
-                 			fontSize: 10,
-                 			bold: true,
-                 			italics: true
-                 		},
-                 		exporterPdfFooter: function (currentPage, pageCount) {
-                 			return {
-                 				text: pageOfPages(currentPage, pageCount),
-                 				style: 'footerStyle'
-                 			};
-                 		},
-                 		exporterPdfCustomFormatter: function (docDefinition) {
-                 			docDefinition.pageMargins = [0, 40, 0, 40];
-                 			docDefinition.styles.headerStyle = {
-                 				fontSize: 22,
-                 				bold: true,
-                 				alignment: 'center',
-                 				lineHeight: 1.5
-                 			};
-                 			docDefinition.styles.footerStyle = {
-                 				fontSize: 10,
-                 				italic: true,
-                 				alignment: 'center'
-                 			};
-                 			return docDefinition;
-                 		},
-                 		exporterPdfOrientation: 'landscape',
-                 		exporterPdfPageSize: 'LETTER',
-                 		exporterPdfMaxGridWidth: 600,
-                 		exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location")),
+        keepLastSelected: true,
+        //useExternalSorting: true,
+        //pagingOptions: $scope.pagingOptions,
+        filterOptions: $scope.filterOptions,
+        sortInfo: $scope.sortOptions,
 
-                 		exporterCsvFilename: 'Passengers.csv',
-                 		exporterPdfHeader: {text: "Passengers", style: 'headerStyle'},
+        columnDefs: [{
+            "name": "ruleHits", "displayName": "H", width: 50, enableFiltering: false,
+            "sort": {
+                direction: uiGridConstants.DESC,
+                priority: 0
+            }
+        },
+            // {"name": "onWatchList", "displayName": "L", width: 50, enableFiltering: false},
+            {
+                "name": "lastName", "displayName": "Last Name", width: 175/*, enableFiltering: false*/,
 
-                         onRegisterApi: function (gridApi) {
-                             $scope.gridApi = gridApi;
-                             
-                             gridApi.selection.on.rowSelectionChanged($scope, function (row) {
-                                 var title;
-                                 if (row.isSelected) {
-                                     $state.go('pax.detail', { id: row.entity.paxId, flightId: row.entity.flightId });
-                                 }
-                             });
-                             
-                             
-                         }
-    
-    
-    	} // END of Passenger Grid
-    	
+                "sort": {
+                    direction: uiGridConstants.DESC,
+                    priority: 1
+                }
+            },
+            {"name": "firstName", "displayName": "First Name", width: 150/*, enableFiltering: false*/},
+            {"name": "middleName", "displayName": "Middle", width: 100/*, enableFiltering: false*/},
+            {"name": "flightNumber", "displayName": "Flight", width: 90/*, enableFiltering: false*/},
+            {"name": "flightETA", "displayName": "ETA", width: 175, enableFiltering: false},
+            {"name": "flightETD", "displayName": "ETD", width: 175, enableFiltering: false},
+            {"name": "passengerType", "displayName": "Type", width: 50, enableFiltering: false},
+            {"name": "gender", "displayName": "G", width: 50, enableFiltering: false},
+            {"name": "dob", "displayName": "DOB", field: 'dob', cellFilter: 'date', width: 175, enableFiltering: false},
+            {"name": "citizenshipCountry", "displayName": "CTZ", width: 75/*, enableFiltering: false*/},
+            {"name": "passengerType", "displayName": "T", width: 100, enableFiltering: false},
+            {"name": "documentType", "displayName": "T", width: 50, enableFiltering: false},
+            {"name": "carrier", "displayName": "Carrier", width: 50/*, enableFiltering: false*/},
+            {"name": "seat", "displayName": "Seat", width: 75, enableFiltering: false}],
+
+        enableGridMenu: true,
+        enableSelectAll: false,
+        exporterPdfDefaultStyle: {fontSize: 9},
+        exporterPdfTableStyle: {margin: [10, 10, 10, 10]},
+        exporterPdfTableHeaderStyle: {
+            fontSize: 10,
+            bold: true,
+            italics: true
+        },
+        exporterPdfFooter: function (currentPage, pageCount) {
+            return {
+                text: pageOfPages(currentPage, pageCount),
+                style: 'footerStyle'
+            };
+        },
+        exporterPdfCustomFormatter: function (docDefinition) {
+            docDefinition.pageMargins = [0, 40, 0, 40];
+            docDefinition.styles.headerStyle = {
+                fontSize: 22,
+                bold: true,
+                alignment: 'center',
+                lineHeight: 1.5
+            };
+            docDefinition.styles.footerStyle = {
+                fontSize: 10,
+                italic: true,
+                alignment: 'center'
+            };
+            return docDefinition;
+        },
+        exporterPdfOrientation: 'landscape',
+        exporterPdfPageSize: 'LETTER',
+        exporterPdfMaxGridWidth: 600,
+        exporterCsvLinkElement: angular.element(document.querySelectorAll(".custom-csv-link-location")),
+
+        exporterCsvFilename: 'Passengers.csv',
+        exporterPdfHeader: {text: "Passengers", style: 'headerStyle'},
+
+        onRegisterApi: function (gridApi) {
+            $scope.gridApi = gridApi;
+
+            gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+                var title;
+                if (row.isSelected) {
+                    $state.go('pax.detail', {id: row.entity.paxId, flightId: row.entity.flightId});
+                }
+            });
 
 
-	    //var pax = {
-         //   startDate: $filter('date')($scope.startDate, 'MM/dd/yyyy'),
-         //   endDate: $filter('date')($scope.endDate, 'MM/dd/yyyy')
-        //
-	    //    };
-	    //
-	    //paxService.getAllPax(pax).then(function (myData) {
-         //  // $scope.loading = false;
-	    //	$scope.passengerGrid.totalItems = myData[0];
-	    //    $scope.passengerGrid.data = myData[1];
-	    //});
+        }
 
-    $scope.refreshListing = function(){
+
+    }; // END of Passenger Grid
+
+    $scope.refreshListing = function () {
         $scope.loading = true;
         $scope.passengerGrid.data = [];
-    	
+
         var pax = {
             startDate: $filter('date')($scope.startDate, 'MM/dd/yyyy'),
             endDate: $filter('date')($scope.endDate, 'MM/dd/yyyy')
-	        };
-	    
-	    paxService.getAllPax(pax).then(function (myData) {
-            $scope.loading = false;
-	    	$scope.passengerGrid.totalItems = myData[0];
-	        $scope.passengerGrid.data = myData[1];
+        };
 
-	    });
-	    
+        paxService.getAllPax(pax).then(function (myData) {
+            $scope.loading = false;
+            $scope.passengerGrid.totalItems = myData[0];
+            $scope.passengerGrid.data = myData[1];
+        });
     };
 
     $scope.refreshListing();
-
 
     //------- Pre-Refactor-------------------
     //Function to get Rule Hits per Passenger
@@ -335,80 +315,95 @@ app.controller('PaxController', function ($scope, $rootScope, $injector, GridCon
             }); // END of paxService getRuleHits
         }
     };
-    
-
-
 }); // END of PaxController
 
 // Customs Filters
 
-app.filter('capitalize', function() {
-  return function(input, all) {
-    return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}) : '';
-  }
+app.filter('capitalize', function () {
+    return function (input, all) {
+        return (!!input) ? input.replace(/([^\W_]+[^\s-]*) */g, function (txt) {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        }) : '';
+    };
 });
 
-app.filter('DobDateFormat', function myDateFormat($filter){
-	  return function(text){
-	    var  tempdate= new Date(text.replace(/-/g,"/"));
-	    return $filter('date')(tempdate, "MMM-dd-yyyy");
-	  }
-	});
+app.filter('DobDateFormat', function myDateFormat($filter) {
+    return function (text) {
+        var tempdate = new Date(text.replace(/-/g, "/"));
+        return $filter('date')(tempdate, "MMM-dd-yyyy");
+    };
+});
 
 
-app.filter('orderObjectBy', function() {
-	  return function(items, field, reverse) {
-	    var filtered = [];
-	    angular.forEach(items, function(item) {
-	      filtered.push(item);
-	    });
-	    filtered.sort(function (a, b) {
-	      return (a[field] > b[field] ? 1 : -1);
-	    });
-	    if(reverse) filtered.reverse();
-	    return filtered;
-	  };
-	});
+app.filter('orderObjectBy', function () {
+    return function (items, field, reverse) {
+        var filtered = [];
+        angular.forEach(items, function (item) {
+            filtered.push(item);
+        });
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+        if (reverse) filtered.reverse();
+        return filtered;
+    };
+});
 
-app.filter('flagImageFilter', function() {
-    return function(hits) {
-        if(hits==='0') { return 'padding: 1.5px; color: #007500;'; }
-        if(hits!='0')  { return 'padding: 1.5px; color: #d9534f;'; }
+app.filter('flagImageFilter', function () {
+    return function (hits) {
+        if (hits === '0') {
+            return 'padding: 1.5px; color: #007500;';
+        }
+        if (hits != '0') {
+            return 'padding: 1.5px; color: #d9534f;';
+        }
         return 'color: #007500;';
     };
-
-})
+});
 
 //flagImageFilter
 
-app.filter('watchListImageFilter', function() {
-    return function(hits) {
-        if(hits===1)  { return 'icon-user-check glyphiconWLPax col-sm-4'; } // glyphiconWLPax
-        if(hits===2)  { return 'icon-book glyphiconWLDocs col-sm-4'; } // glyphiconWLDocs
-        if(hits===3)  { return 'icon-user-check glyphiconWLPaxDocs col-sm-4'; } // glyphiconWLPaxDocs  glyphicon-user
+app.filter('watchListImageFilter', function () {
+    return function (hits) {
+        if (hits === 1) {
+            return 'icon-user-check glyphiconWLPax col-sm-4';
+        } // glyphiconWLPax
+        if (hits === 2) {
+            return 'icon-book glyphiconWLDocs col-sm-4';
+        } // glyphiconWLDocs
+        if (hits === 3) {
+            return 'icon-user-check glyphiconWLPaxDocs col-sm-4';
+        } // glyphiconWLPaxDocs  glyphicon-user
         return '';
     };
+});
 
-})
-
-app.filter('watchListImageInsertFilter', function() {
-    return function(hits) {
-        if(hits===1)  { return ''; } // glyphiconWLPax
-        if(hits===2)  { return ''; } // glyphiconWLDocs
-        if(hits===3)  { return 'icon-book'; } // glyphiconWLPaxDocs
+app.filter('watchListImageInsertFilter', function () {
+    return function (hits) {
+        if (hits === 1) {
+            return '';
+        } // glyphiconWLPax
+        if (hits === 2) {
+            return '';
+        } // glyphiconWLDocs
+        if (hits === 3) {
+            return 'icon-book';
+        } // glyphiconWLPaxDocs
         return '';
     };
+});
 
-})
-
-app.filter('watchListImageColorFilter', function() {
-    return function(hits) {
-        if(hits===1)  { return 'padding: 7.5px; color: #d9534f;'; } // glyphiconWLPax
-        if(hits===2)  { return 'padding: 7.5px; color: #d9534f;'; } // glyphiconWLDocs
-        if(hits===3)  { return 'padding: 7.5px; color: #d9534f;'; } // glyphiconWLPaxDocs
+app.filter('watchListImageColorFilter', function () {
+    return function (hits) {
+        if (hits === 1) {
+            return 'padding: 7.5px; color: #d9534f;';
+        } // glyphiconWLPax
+        if (hits === 2) {
+            return 'padding: 7.5px; color: #d9534f;';
+        } // glyphiconWLDocs
+        if (hits === 3) {
+            return 'padding: 7.5px; color: #d9534f;';
+        } // glyphiconWLPaxDocs
         return '';
     };
-
-})
-
-
+});
--- gtas-parent/gtas-webapp/src/main/webapp/pax/PaxService.js
+++ gtas-parent/gtas-webapp/src/main/webapp/pax/PaxService.js
@@ -1,102 +1,88 @@
-app.service("paxService", function( $rootScope, $http, $q ) {	
-	// Return public API.
-	return({
-	    getPax: getPax,
-        broadcast: broadcast,
-        getRuleHits: getRuleHits,
-        broadcastRuleID: broadcastRuleID,
-        getAllPax: getAllPax,
-        getPaxByPage: getPaxByPage
-	});
-	
-	function getPax(flightId) {
-	    var request = $http({
-	        method: "get",
-	        url: "/gtas/passengers" + (flightId ? "?flightId=" + flightId : ""),
-	        params: {
-	            action: "get"
-	        }
-	    });
-	    return( request.then( handleSuccess, handleError ) );
-	}
-	
-    function handleError( response ) {
-        if (! angular.isObject( response.data ) || ! response.data.message) {
-            return( $q.reject( "An unknown error occurred." ) );
-        }
-        return( $q.reject( response.data.message ) );
+app.service("paxService", function ($rootScope, $http, $q) {
+    'use strict';
+    function handleSuccess(response) {
+        return (response.data);
     }
 
-    function handleSuccess( response ) {
-        return( response.data );
+    function handleError(response) {
+        if (!angular.isObject(response.data) || !response.data.message) {
+            return ($q.reject("An unknown error occurred."));
+        }
+        return ($q.reject(response.data.message) );
     }
-    
+
+    function getPax(flightId) {
+        var request = $http({
+            method: "get",
+            url: "/gtas/passengers" + (flightId ? "?flightId=" + flightId : ""),
+            params: {
+                action: "get"
+            }
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+
     function broadcast(flightId) {
-        $rootScope.$broadcast('handleBroadcast', flightId); 
-    }    
-    
-    
+        $rootScope.$broadcast('handleBroadcast', flightId);
+    }
+
+
     function getRuleHits(passengerId) {
-	    var request = $http({
-	        method: "get",
-	        url: "/gtas/hit/passenger" + (passengerId ? "?passengerId=" + passengerId : ""),
-	        params: {
-	            action: "get"
-	        }
-	    });
-	    return( request.then( handleSuccess, handleError ) );
-    	
+        var request = $http({
+            method: "get",
+            url: "/gtas/hit/passenger" + (passengerId ? "?passengerId=" + passengerId : ""),
+            params: {
+                action: "get"
+            }
+        });
+        return (request.then(handleSuccess, handleError));
+
     }
-    
+
     function broadcastRuleID(ruleID) {
-        $rootScope.$broadcast('ruleIDBroadcast', ruleID); 
+        $rootScope.$broadcast('ruleIDBroadcast', ruleID);
     }
-    
-    
-	function getAllPax() {
-	    var request = $http({
-	        method: "get",
-	        url: "/gtas/passengers/all",
-	        params: {
-	            action: "get"
-	        }
-	    });
-	    return( request.then( handleSuccess, handleError ) );
-	}
-    
-	function getAllPax(pax) {
-	    var request = $http({
-	        method: "get",
-	        url: "/gtas/passengers/all",
-	        params: pax
-	    });
-	    return( request.then( handleSuccess, handleError ) );
-	}
-	
-	function getPaxByPage(pax) {
-	    var request = $http({
-	        method: "get",
-	        url: "/gtas/passengers/page/"+pax.pageNumber,
-	        params: pax
+
+    function getAllPax(pax) {
+        var request = $http({
+            method: "get",
+            url: "/gtas/passengers/all",
+            params: pax
+        });
+        return (request.then(handleSuccess, handleError));
+    }
+
+    function getPaxByPage(pax) {
+        var request = $http({
+            method: "get",
+            url: "/gtas/passengers/page/" + pax.pageNumber,
+            params: pax
 //	        {
 //	            action: "get",
 //	            pageNumber: pageNumber,
 //	            name: ""
 //	        }
-	    });
-	    return( request.then( handleSuccess, handleError ) );
-	}
-});
-
-app.service("paxDetailService",['$http','$stateParams', function($http, $stateParams){
-return({getPaxDetail: getPaxDetail})
-
-	function getPaxDetail($http, $stateParams){
-        var url = "/gtas/passengers/" + $stateParams.id+"?flightId=" + $stateParams.flightId;
-        return $http.get(url);
-            //.then(function(res){ return res.data; });
+        });
+        return (request.then(handleSuccess, handleError));
     }
 
+    // Return public API.
+    return ({
+        getPax: getPax,
+        broadcast: broadcast,
+        getRuleHits: getRuleHits,
+        broadcastRuleID: broadcastRuleID,
+        getAllPax: getAllPax,
+        getPaxByPage: getPaxByPage
+    });
+});
+
+app.service("paxDetailService", ['$http', '$stateParams', function ($http, $stateParams) {
+    'use strict';
+    var getPaxDetail = function ($http, $stateParams) {
+        var url = "/gtas/passengers/" + $stateParams.id + "?flightId=" + $stateParams.flightId;
+        return $http.get(url);
+        //.then(function(res){ return res.data; });
+    };
+    return ({getPaxDetail: getPaxDetail});
 }]);
-
-
