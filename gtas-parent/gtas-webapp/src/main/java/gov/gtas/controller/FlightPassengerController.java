407d8ab5853f6a3b75c7a1f0378a8c8a4af70c06
Michael Copenhafer <mcopenhafer@rockcreek.local>
2015-09-24 14:14:17 -0400

    removing deprecated pax rest api

--- gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/FlightPassengerController.java
+++ gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/FlightPassengerController.java
@@ -9,13 +9,13 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import gov.gtas.model.Document;
 import gov.gtas.model.Flight;
@@ -26,7 +26,7 @@ import gov.gtas.vo.passenger.DocumentVo;
 import gov.gtas.vo.passenger.FlightVo;
 import gov.gtas.vo.passenger.PassengerVo;
 
-@Controller
+@RestController
 public class FlightPassengerController {
 	private static final Logger logger = LoggerFactory.getLogger(FlightPassengerController.class);
 
