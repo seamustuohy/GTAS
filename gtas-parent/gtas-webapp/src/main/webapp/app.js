22ce0f77b70a604bcad3df7c0ae2fd222e1217b4
Michael Copenhafer <mcopenhafer@rockcreek.local>
2015-09-25 08:49:37 -0400

    removing ui bootstrap

--- gtas-parent/gtas-webapp/src/main/webapp/app.js
+++ gtas-parent/gtas-webapp/src/main/webapp/app.js
@@ -11,7 +11,6 @@ var app = angular.module('myApp', [
     'ui.grid.selection',
     'ui.grid.exporter',
     'ui.grid.expandable',
-    'ui.bootstrap',
     'ngMaterial',
     'ngMessages',
     'ngAria',
