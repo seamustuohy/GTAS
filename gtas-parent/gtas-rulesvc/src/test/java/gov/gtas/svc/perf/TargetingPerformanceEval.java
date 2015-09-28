445864e4855d112fe7ccb211f2dfc77283f8df40
Amit <amit_bandopadhay@yahoo.com>
2015-09-27 23:15:09 -0400

    refactor performance tests

--- gtas-parent/gtas-rulesvc/src/test/java/gov/gtas/svc/perf/TargetingPerformanceEval.java
+++ gtas-parent/gtas-rulesvc/src/test/java/gov/gtas/svc/perf/TargetingPerformanceEval.java
@@ -3,11 +3,6 @@ package gov.gtas.svc.perf;
 import gov.gtas.config.RuleServiceConfig;
 import gov.gtas.enumtype.EntityEnum;
 import gov.gtas.model.MessageStatus;
-import gov.gtas.model.watchlist.json.WatchlistItemSpec;
-import gov.gtas.model.watchlist.json.WatchlistSpec;
-import gov.gtas.model.watchlist.json.WatchlistTerm;
-import gov.gtas.querybuilder.mappings.DocumentMapping;
-import gov.gtas.querybuilder.mappings.PassengerMapping;
 import gov.gtas.repository.ApisMessageRepository;
 import gov.gtas.services.watchlist.WatchlistPersistenceService;
 import gov.gtas.svc.TargetingService;
@@ -19,6 +14,7 @@ import javax.transaction.Transactional;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -35,29 +31,9 @@ import org.springframework.test.context.transaction.TransactionConfiguration;
 @ContextConfiguration(classes = RuleServiceConfig.class)
 @TransactionConfiguration(defaultRollback = false)
 public class TargetingPerformanceEval {
-	public static final String WL_AUTHOR = "bstygar";
 	public static final String PASSENGER_WL_NAME = "PerfTest Passenger WL";
 	public static final String DOCUMENT_WL_NAME = "PerfTest Document WL";
 	
-	public static final String[][] WL_DATA = {
-		{"Jonathon", "Smith", "1964-07-12", "123456789"},
-		{"Williamnevell", "Jones", "1964-07-12", "123456789"},
-		{"Johnalva", "Wayne", "2012-07-12", "123456789"},
-		{"Garrywilliam", "Cooper", "2015-07-12", "987654321"},
-		{"Jonthon", "Smith", "1964-07-12", "548721687"},
-		{"Jothon", "Smith", "1964-07-12", "159264375"},
-		{"Jonaon", "Smith", "1964-07-12", "12356789"},
-		{"Jothon", "Smith", "1964-07-12", "12346789"},
-		{"Jonahon", "Smith", "1964-07-12", "12356789"},
-		{"Jonathon", "Smith", "1964-07-12", "12346789"},
-		{"Jonatn", "Smith", "1964-07-12", "12345689"},
-		{"Jonaton", "Smith", "1964-07-12", "12345689"},
-		{"Jonaton", "Smith", "1964-07-12", "12345789"},
-		{"Jonaton", "Smith", "1964-07-12", "12345789"},
-		{"Jonaon", "Smith", "1964-07-12", "12345789"}
-		
-	};
-
 	@Autowired
 	TargetingService targetingService;
 
@@ -78,51 +54,11 @@ public class TargetingPerformanceEval {
 	public void tearDown() throws Exception {
 	}
 
-	private void generateWlRules(String wlName, EntityEnum entity,
-			String[][] dataArr, int count) {
-		for(int i = 0; i < count; ++i) {
-			WatchlistSpec spec = new WatchlistSpec(wlName, entity.getEntityName());
-			for (String[] data : dataArr) {
-				spec.addWatchlistItem(createItem(entity, null, "create", data));
-			}
-			watchlistService.createOrUpdateWatchlist(WL_AUTHOR, spec);
-		}
-	}
-
-	private WatchlistItemSpec createItem(EntityEnum entity, Long id,
-			String action, String[] data) {
-		WatchlistTerm[] terms = null;
-		switch (entity) {
-		case PASSENGER:
-			terms = new WatchlistTerm[3];
-			terms[0] = new WatchlistTerm(entity.getEntityName(),
-					PassengerMapping.FIRST_NAME.getFieldName(),
-					PassengerMapping.FIRST_NAME.getFieldType(), data[0]);
-			terms[1] = new WatchlistTerm(entity.getEntityName(),
-					PassengerMapping.LAST_NAME.getFieldName(),
-					PassengerMapping.LAST_NAME.getFieldType(), data[1]);
-			terms[2] = new WatchlistTerm(entity.getEntityName(),
-					PassengerMapping.DOB.getFieldName(),
-					PassengerMapping.DOB.getFieldType(), data[2]);
-			break;
-		case DOCUMENT:
-			terms = new WatchlistTerm[1];
-			terms[0] = new WatchlistTerm(entity.getEntityName(),
-					DocumentMapping.DOCUMENT_NUMBER.getFieldName(),
-					DocumentMapping.DOCUMENT_NUMBER.getFieldType(), data[3]);
-		default:
-			break;
-		}
-		return new WatchlistItemSpec(id, action, terms);
-	}
-	
-	
-
 	//@Test
 	@Transactional
 	public void genPerformanceData() {
-		generateWlRules(PASSENGER_WL_NAME, EntityEnum.PASSENGER, WL_DATA, 100);
-		generateWlRules(DOCUMENT_WL_NAME, EntityEnum.DOCUMENT, WL_DATA, 20);
+		WatchlistRuleGenerator.generateWlRules(watchlistService, PASSENGER_WL_NAME, EntityEnum.PASSENGER, 100);
+		WatchlistRuleGenerator.generateWlRules(watchlistService, DOCUMENT_WL_NAME, EntityEnum.DOCUMENT, 20);
 		System.out.println("*****************************************************************");
 		System.out.println("********************   GENERATION COMPLETE  *********************");
 		System.out.println("*****************************************************************");
@@ -134,7 +70,7 @@ public class TargetingPerformanceEval {
 		System.out.println("********************   ACTIVATION COMPLETE  *********************");
 		System.out.println("*****************************************************************");
 	}
-	//@Test
+	@Test
 	@Transactional
 	public void runPerformance() {
 		long start = System.currentTimeMillis();
