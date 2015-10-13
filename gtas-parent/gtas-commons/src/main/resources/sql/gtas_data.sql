692b6077dbc38c77c0b0df9997baceac5dca5424
Michael Copenhafer <mcopenhafer@rockcreek.local>
2015-10-13 12:31:53 -0400

    added a user for Nael

--- gtas-parent/gtas-commons/src/main/resources/sql/gtas_data.sql
+++ gtas-parent/gtas-commons/src/main/resources/sql/gtas_data.sql
@@ -11,17 +11,19 @@ INSERT INTO `role` VALUES ('5', 'Manage Rules');
 -- Records of user
 -- ----------------------------
 -- password is 'password'
+INSERT INTO `user` VALUES ('nsamha', 1,'Nael', 'Samha', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
+INSERT INTO `user` VALUES ('bstygar', 1,'Brian', 'Stygar', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
 INSERT INTO `user` VALUES ('svempati', 1,'Srinivas', 'Vempati', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
 INSERT INTO `user` VALUES ('abandopadhay',1, 'Amit', 'Bandopadhay', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
 INSERT INTO `user` VALUES ('jtang', 1,'Jeen', 'Tang', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
 INSERT INTO `user` VALUES ('mcopenhafer',1, 'Mike', 'Copenhafer', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
-INSERT INTO `user` VALUES ('bstygar', 1,'Brian', 'Stygar', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
 INSERT INTO `user` VALUES ('ladebiyi',1, 'Lola', 'Adebiyi', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
 INSERT INTO `user` VALUES ('adelorie',1, 'Anthony', 'Delorie', '$2a$10$T5xL/wIet8ev/RhqM5iDTOrqRwIbO7IkQ/DWkDe592zaBaZy8RAxu');
 
 -- ----------------------------
 -- Records of user_role
 -- ----------------------------
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('nsamha', 1);
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('bstygar', 1);
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('svempati', 1);
 INSERT INTO `user_role` (`user_id`, `role_id`) VALUES ('abandopadhay', 1);
