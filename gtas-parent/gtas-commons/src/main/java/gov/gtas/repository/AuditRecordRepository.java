f830d571520ed1ffe58a1c2e8e3006de288fbfe1
Amit <amit_bandopadhay@yahoo.com>
2015-10-19 22:59:12 -0400

    audit log implementation + tests

---
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/repository/AuditRecordRepository.java
@@ -0,0 +1,24 @@
+package gov.gtas.repository;
+
+import gov.gtas.enumtype.AuditActionType;
+import gov.gtas.model.AuditRecord;
+import gov.gtas.model.User;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface AuditRecordRepository extends CrudRepository<AuditRecord, Long>{
+	public List<AuditRecord> findByActionType(AuditActionType actionType);
+	public List<AuditRecord> findByUser(User user);
+	public List<AuditRecord> findByUserAndActionType(User user, AuditActionType actionType);
+	
+	@Query("SELECT ar FROM AuditRecord ar WHERE ar.timestamp >= :fromDate and  ar.timestamp <= :toDate")
+    public List<AuditRecord> findByTimestampRange(@Param("fromDate") Date fromDate, @Param("toDate") Date toDate);  
+	
+	@Query("SELECT ar FROM AuditRecord ar WHERE ar.timestamp >= :fromDate")
+    public List<AuditRecord> findByTimestampFrom(@Param("fromDate") Date fromDate);   
+}
---
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/services/AuditLogPersistenceService.java
@@ -0,0 +1,18 @@
+package gov.gtas.services;
+
+import gov.gtas.enumtype.AuditActionType;
+import gov.gtas.model.AuditRecord;
+
+import java.util.Date;
+import java.util.List;
+
+public interface AuditLogPersistenceService {
+	public AuditRecord create(AuditRecord aRec);
+    public AuditRecord findById(Long id);
+
+    public List<AuditRecord> findByDateRange(Date dateFrom, Date dateTo);
+    public List<AuditRecord> findByDateFrom(Date dateFrom);
+    public List<AuditRecord> findByUser(String userId);
+    public List<AuditRecord> findByActionType(AuditActionType type);
+    public List<AuditRecord> findByUserAndActionType(AuditActionType type, String user);
+}
