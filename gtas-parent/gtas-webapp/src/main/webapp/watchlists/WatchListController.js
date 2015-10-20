b1d386b3a1e00d9b707d6c0c79dabddc253211cc
Anthony Delorie <de.lorie@gmail.com>
2015-10-20 11:24:25 -0400

    removed <<<HEAD

--- gtas-parent/gtas-webapp/src/main/webapp/watchlists/WatchListController.js
+++ gtas-parent/gtas-webapp/src/main/webapp/watchlists/WatchListController.js
@@ -1,6 +1,5 @@
 (function () {
     'use strict';
-<<<<<<< HEAD
     app.controller('WatchListController', function ($scope, gridOptionsLookupService, $q, watchListService, $interval, spinnerService, $timeout) {
         var watchlist = {},
             tabs = [],
