4457d986c5c28af8489ea99147612269329aad98
jtangGitHub <jtangGitHub@gmail.com>
2015-10-23 10:08:32 -0400

    update logging info

--- gtas-parent/gtas-rulesvc/src/main/java/gov/gtas/rule/listener/GtasAgendaEventListener.java
+++ gtas-parent/gtas-rulesvc/src/main/java/gov/gtas/rule/listener/GtasAgendaEventListener.java
@@ -2,18 +2,30 @@ package gov.gtas.rule.listener;
 
 import gov.gtas.bo.RuleExecutionStatistics;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.kie.api.definition.rule.Rule;
 import org.kie.api.event.rule.AfterMatchFiredEvent;
 import org.kie.api.event.rule.DefaultAgendaEventListener;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * This class extends DefaultAgendaEventListener that 
- * implements AgendaEventListener interface with empty methods so that you only have to 
+ * This class extends DefaultAgendaEventListener that implements
+ * AgendaEventListener interface with empty methods so that you only have to
  * override the methods that you are interested in
  * 
  *
  */
 public class GtasAgendaEventListener extends DefaultAgendaEventListener {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(GtasAgendaEventListener.class);
+
+	private List<String> ruleNameList = new ArrayList<String>();
+
 	private RuleExecutionStatistics ruleExecutionStatistics;
 
 	/**
@@ -30,6 +42,50 @@ public class GtasAgendaEventListener extends DefaultAgendaEventListener {
 		this.ruleExecutionStatistics.incrementTotalRulesFired();
 		this.ruleExecutionStatistics.addRuleFired(event.getMatch().getRule()
 				.getName());
+
+		Rule rule = event.getMatch().getRule();
+		String ruleName = rule.getName();
+		Map<String, Object> ruleMetaDataMap = rule.getMetaData();
+
+		ruleNameList.add(ruleName);
+		StringBuilder sb = new StringBuilder("Rule fired: " + ruleName);
+
+		if (ruleMetaDataMap.size() > 0) {
+			sb.append("\n  With [" + ruleMetaDataMap.size() + "] meta-data:");
+			for (String key : ruleMetaDataMap.keySet()) {
+				sb.append("\n    key=" + key + ", value="
+						+ ruleMetaDataMap.get(key));
+			}
+		}
+
+		logger.debug(sb.toString());
+	}
+	public boolean isRuleFired(String ruleName) {
+		for (String a : ruleNameList) {
+			if (a.equals(ruleName)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
+	public void reset() {
+		ruleNameList.clear();
+	}
+
+	public final List<String> getRuleNameList() {
+		return ruleNameList;
+	}
+
+	public String ruleMatchesToString() {
+		if (ruleNameList.size() == 0) {
+			return "No matches occurred.";
+		} else {
+			StringBuilder sb = new StringBuilder("Rules Matched: ");
+			for (String match : ruleNameList) {
+				sb.append("\n  rule: ").append(match);
+			}
+			return sb.toString();
+		}
+	}
 }
--- gtas-parent/gtas-rulesvc/src/main/java/gov/gtas/rule/listener/GtasRuleRuntimeEventListener.java
+++ gtas-parent/gtas-rulesvc/src/main/java/gov/gtas/rule/listener/GtasRuleRuntimeEventListener.java
@@ -2,20 +2,30 @@ package gov.gtas.rule.listener;
 
 import gov.gtas.bo.RuleExecutionStatistics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.kie.api.event.rule.DefaultRuleRuntimeEventListener;
 import org.kie.api.event.rule.ObjectDeletedEvent;
 import org.kie.api.event.rule.ObjectInsertedEvent;
 import org.kie.api.event.rule.ObjectUpdatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- * This class extends DefaultRuleRuntimeEventListener that 
- * implements RuleRuntimeEventListener interface with empty methods so that you only have to 
- * override the methods that you are interested in
+ * This class extends DefaultRuleRuntimeEventListener that implements
+ * RuleRuntimeEventListener interface with empty methods so that you only have
+ * to override the methods that you are interested in
  * 
  *
  */
 public class GtasRuleRuntimeEventListener extends
 		DefaultRuleRuntimeEventListener {
+	private static final Logger logger = LoggerFactory
+			.getLogger(GtasRuleRuntimeEventListener.class);
+	private static final List<ObjectInsertedEvent> insertEvents = new ArrayList<ObjectInsertedEvent>();
+	private static final List<ObjectUpdatedEvent> updateEvents = new ArrayList<ObjectUpdatedEvent>();
+	private static final List<ObjectDeletedEvent> retractEvents = new ArrayList<ObjectDeletedEvent>();
 
 	private RuleExecutionStatistics ruleExecutionStatistics;
 
@@ -32,16 +42,31 @@ public class GtasRuleRuntimeEventListener extends
 	public void objectInserted(ObjectInsertedEvent event) {
 		this.ruleExecutionStatistics.incrementTotalObjectsModified();
 		this.ruleExecutionStatistics.addInsertedObject(event.getObject());
+		//insertEvents.add(event);
 	}
 
 	public void objectUpdated(ObjectUpdatedEvent event) {
 		this.ruleExecutionStatistics.incrementTotalObjectsModified();
 		this.ruleExecutionStatistics.addModifiedObject(event.getObject());
+		//updateEvents.add(event);
 	}
 
 	public void objectDeleted(ObjectDeletedEvent event) {
 		this.ruleExecutionStatistics.incrementTotalObjectsModified();
 		this.ruleExecutionStatistics.addDeletedObject(event.getOldObject());
+		//retractEvents.add(event);
+	}
+
+	public static List<ObjectInsertedEvent> getInsertevents() {
+		return insertEvents;
+	}
+
+	public static List<ObjectUpdatedEvent> getUpdateevents() {
+		return updateEvents;
+	}
+
+	public static List<ObjectDeletedEvent> getRetractevents() {
+		return retractEvents;
 	}
 
 }
