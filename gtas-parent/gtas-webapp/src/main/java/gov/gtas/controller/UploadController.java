172ea5f7634e1434fa12030cd55c1e9dd4036ef4
Michael Copenhafer <mcopenhafer@rockcreek.lan>
2015-10-28 13:51:33 -0400

    preliminary lookup table for app config options like home country, directories, etc.  requires db refresh

--- gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/UploadController.java
+++ gtas-parent/gtas-webapp/src/main/java/gov/gtas/controller/UploadController.java
@@ -1,7 +1,10 @@
 package gov.gtas.controller;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +18,11 @@ public class UploadController {
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/upload")
     public void upload(@RequestParam("file") MultipartFile file, @RequestParam("username") String username ) throws IOException {
-
         byte[] bytes;
-
         if (!file.isEmpty()) {
              bytes = file.getBytes();
-            //store file in storage
+//             FileOutputStream output = new FileOutputStream(new File("target-file"));
+//             IOUtils.write(bytes, output);
         }
 
         System.out.println(String.format("receive %s from %s", file.getOriginalFilename(), username));
