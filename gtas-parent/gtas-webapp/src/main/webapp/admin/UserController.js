6463db7a8955b49156726264a50c136fbc69aa8c
Anthony Delorie <de.lorie@gmail.com>
2015-11-03 11:10:36 -0500

    removed several scope models/methods, simplified into 1/3rd the code, only one loop at the end to send selected roles, simplified admin toggle disabling, deselecting other roles

--- gtas-parent/gtas-webapp/src/main/webapp/admin/UserController.js
+++ gtas-parent/gtas-webapp/src/main/webapp/admin/UserController.js
@@ -1,215 +1,97 @@
 app.controller('UserCtrl', function ($scope, $stateParams, userService, $mdToast, $location) {
     'use strict';
-    var backToAdmin = function (data) {
-            console.log(data);
-            $location.path('/admin');
-        },
-        SelectedRole = function (role, selected, disabled) {
-            this.role = role;
-            this.selected = selected;
-            this.disabled = disabled;
-        },
-        enableRoles = function () {
-            $scope.user.roles.forEach(function (obj) {
-                if (obj.roleDescription !== 'Admin') {
-                    $scope.roles[obj.roleDescription] = {
-                        role: obj.roleDescription,
-                        selected: true,
-                        disabled: false
-                    };
-                } else if (obj.roleDescription === 'Admin') {
-                    $scope.roles[obj.roleDescription] = {
-                        role: obj.roleDescription,
-                        selected: true,
-                        disabled: false
-                    };
-                    $scope.rolesLookup.forEach(function (roleLookup) {
-                        if (roleLookup.roleDescription !== obj.roleDescription) {
-                            $scope.roles[roleLookup.roleDescription] = {
-                                role: roleLookup.roleDescription,
-                                selected: false,
-                                disabled: false
-                            };
-                        }
-                    });
+    var backToAdmin = function () { $location.path('/admin'); },
+        setNonAdminRoles = function (adminSelectedState) {
+            $scope.roles.forEach(function (role) {
+                if (role.roleDescription !== 'Admin') {
+                    role.disabled = adminSelectedState;
+                    if (adminSelectedState) {
+                        role.selected = false;
+                    }
                 }
             });
         },
         setUser = function (users) {
             users.forEach(function (user) {
                 if (user.userId === $stateParams.userId) {
+                    var userSelectedRoles = user.roles.map(function (role) { return role.roleDescription; }),
+                        admin = $scope.roles[3]; //not sure why this is 4th...
                     $scope.user = user;
+                    $scope.roles.forEach(function (role) {
+                        if (userSelectedRoles.indexOf(role.roleDescription) >= 0) {
+                            role.selected = true;
+                        }
+                    });
+                    setNonAdminRoles(admin.selected);
                 }
             });
-            $scope.persistUser = $scope.user;
-            $scope.persistUser.activeName = $scope.persistUser.userId;
-            $scope.persistUser.activeWord = $scope.persistUser.password;
-            $scope.userStatus.enabled = $scope.user.active === 1;
-            enableRoles();
-        };
-    $scope.userStatus = {
-        enabled: true
-    };
-    $scope.action = $stateParams.userId === 'new' ? 'create' : 'modify';
-    $scope.userPasswordChanged = false;
-    $scope.persistUser = {
-        activeName: '',
-        activeWord: '',
-        userId: '',
-        firstName: '',
-        lastName: '',
-        active: 1,
-        roles: []
-    };
-
-    $scope.roles = [];
-    $scope.rolesLookup = [];
-    $scope.selectedRoles = [];
-
-    $scope.radioData = [
-        {label: 'Enable', value: 1},
-        {label: 'Disable', value: 0}
-    ];
-
-    //$scope.enableDisableUser = function (value) {
-    //    $scope.persistUser.active = value === true ? 1 : 0;
-    //};
-
-    $scope.Init = function () {
-        userService.getRoles()
-            .then(function (roles) {
-                // responses are redirected to confirmation page nothing do here..
-                $scope.rolesLookup = roles;
-                $scope.setRoles();
-            });
-
-        if ($scope.action === 'modify') {
-            userService.getAllUsers().then(setUser);
-        }
-    };
-
-    $scope.refreshSelectedRoles = function (sourceRoles, targetRoles) {
-        var roleExists;
-        sourceRoles.forEach(function (item) {
-            roleExists = false;
-            targetRoles.forEach(function (targetRole) {
-                if (targetRole.role === item) {
-                    roleExists = true;
-                }
-            });
-            if (!roleExists) {
-                $scope.selectedRoles.push(new SelectedRole(item, true, false));
+            $scope.action = $scope.user === undefined ? 'create' : 'modify';
+            if ($scope.action === 'modify') {
+                $scope.persistUser = $scope.user;
             }
-        });
-    };
+        },
+        getSelectedRoles = function () {
+            var selectedRoles = [];
+            $scope.roles.forEach(function (roleObject) {
+                if (roleObject.selected && !(roleObject.disabled)) {
+                    selectedRoles.push({
+                        roleId: roleObject.roleId,
+                        roleDescription: roleObject.roleDescription
+                    });
+                }
+            });
 
-    $scope.sortRoles = function (items, field, reverse) {
-        var filtered = [];
-        items.forEach(function (item) {
-            filtered.push(item);
-        });
-        filtered.sort(function (a, b) {
-            return (a[field] > b[field] ? 1 : -1);
-        });
-        if (reverse) {
-            filtered.reverse();
-        }
-        return filtered;
-    };
-
-    $scope.selectedRoles = [];
-
-    $scope.toggleRole = function (item) {
-        if (item === 'Admin') {
-            if ($scope.roles[item].selected === true) {
-                $scope.rolesLookup.forEach(function (obj) {
-                    if (obj.roleDescription !== item) {
-                        $scope.roles[obj.roleDescription] = {
-                            role: obj.roleDescription,
-                            selected: false,
-                            disabled: true
-                        };
-                    }
+            return selectedRoles;
+        },
+        alertUser = function (content) {
+            $mdToast.show(
+                $mdToast.simple()
+                    .content(content)
+                    .position("top right")
+                    .hideDelay(3000)
+            );
+        },
+        scopeRoles = function (roles) {
+            $scope.roles = [];
+            roles.forEach(function (obj) {
+                $scope.roles.push({
+                    roleDescription: obj.roleDescription,
+                    roleId: obj.roleId,
+                    selected: false,
+                    disabled: false
                 });
-                return;
-            }
-            if ($scope.roles[item].selected === false) {
-                $scope.roles[item] = {role: 'Admin', selected: false, disabled: false};
-                $scope.setRoles();
-                return;
-            }
+            });
+        };
+
+    $scope.persistUser = { password: '', userId: '', firstName: '', lastName: '', active: 1 };
+    $scope.roles = [];
+    $scope.Init = function () {
+        userService.getRoles().then(scopeRoles);
+        userService.getAllUsers().then(setUser);
+    };
+
+    $scope.toggleRole = function (role) {
+        if (role.roleDescription === 'Admin') {
+            setNonAdminRoles(role.selected);
         }
-        if (item !== 'Admin') {
-            $scope.roles['Admin'] = {role: 'Admin', selected: false, disabled: false};
+    };
+
+    $scope.saveUser = function () {
+        if ($scope.persistUser.userId !== null && $scope.persistUser.password !== null) {
+            $scope.persistUser.userId = $scope.persistUser.userId.trim();
+            $scope.persistUser.password = $scope.persistUser.password.trim();
         }
-    };
-
-    $scope.getRoleFromLookup = function (roleDescription) {
-        $scope.rolesLookup.forEach(function (obj) {
-            if (obj.roleDescription === roleDescription) {
-                return obj;
-            }
-        });
-        return null;
-    };
-
-    $scope.setRoles = function () {
-        $scope.rolesLookup.forEach(function (obj) {
-            $scope.roles[obj.roleDescription] = {
-                role: obj.roleDescription,
-                selected: false,
-                disabled: false
-            };
-            //$scope.selectedRoles.push(new SelectedRole($scope.rolesLookup[i].roleDescription, false, false));
-        });
-    };
-
-    $scope.populateSelectedRoles = function () {
-        var roleObject;
-        $scope.rolesLookup.forEach(function (obj) {
-            roleObject = $scope.roles[obj.roleDescription];
-            if (roleObject.selected) {
-                $scope.persistUser.roles.push($scope.getRoleFromLookup(roleObject.role));
-            }
-        });
-    };
-
-    $scope.encodePassword = function () {
-        $scope.userPasswordChanged = true;
-    };
-
-    $scope.Init();
-
-    $scope.showRoleToast = function () {
-        $mdToast.show(
-            $mdToast.simple()
-                .content('One or More User Roles Have To Be Selected')
-                .position("top right")
-                .hideDelay(3000)
-        );
-    };
-
-    $scope.showFieldToast = function () {
-        $mdToast.show(
-            $mdToast.simple()
-                .content('All Input Fields are Required')
-                .position("top right")
-                .hideDelay(3000)
-        );
-    };
-
-    $scope.saveUser = function (event) {
-        if ($scope.persistUser.activeName !== undefined && $scope.persistUser.activeName !== null && $scope.persistUser.activeWord !== undefined && $scope.persistUser.activeWord !== null) {
-            $scope.persistUser.userId = $scope.persistUser.activeName.replace(/\s+/g, '');
-            $scope.persistUser.password = $scope.persistUser.activeWord.replace(/\s+/g, '');
+        $scope.persistUser.roles = getSelectedRoles();
+        if ($scope.persistUser.userId.length === 0 || $scope.persistUser.password.length === 0) {
+            alertUser('userId and | or password cannot be blank space(s)');
+            return;
         }
-
-        $scope.populateSelectedRoles();
         if ($scope.persistUser.roles.length === 0) {
-            $scope.showRoleToast();
-            event.preventDefault();
+            alertUser('One or More User Roles Have To Be Selected');
             return;
         }
         userService[$scope.action === 'modify' ? 'updateUser' : 'createUser']($scope.persistUser).then(backToAdmin);
     };
+
+    $scope.Init();
 });
