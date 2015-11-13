9154e3ac0f3e5bcbc3fa8bdd1c40800fd0d292a0
Michael Copenhafer <mcopenhafer@rockcreek.local>
2015-11-13 13:52:07 -0500

    grab APIS seat info, if it exists

--- gtas-parent/gtas-parsers/src/main/java/gov/gtas/parsers/paxlst/segment/unedifact/RFF.java
+++ gtas-parent/gtas-parsers/src/main/java/gov/gtas/parsers/paxlst/segment/unedifact/RFF.java
@@ -2,49 +2,69 @@ package gov.gtas.parsers.paxlst.segment.unedifact;
 
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+
 import gov.gtas.parsers.edifact.Composite;
 import gov.gtas.parsers.edifact.Segment;
 
 /**
  * <p>
  * RFF: REFERENCE
- * <p>
- * The use of this segment is Mandatory for Secure Flight. The value sent by the
- * aircraft operator system in data element C506:1153 below will be returned to
- * the aircraft operator CUSRES message to facilitate the reconciliation of the
- * messages exchanged. Additionally, the numeric value in data element C506:1153
- * may be used to sequence any follow-on messages related to updates applied to
- * the same passenger manifest. This value will also be returned in the CURES
- * response message. ￼
- * <p>
- * Example: RFF+TN:OZ56789034:::2’ Indicates transaction reference number
- * OZ56789034 assigned by an airline system. The Revision Identifier may
- * optionally be used to identify this passenger data submission as the second
- * submission for this passenger (i.e updated passenger data).
  */
 public class RFF extends Segment {
-    private String referenceCodeQualifier;
+    public enum RffCode {
+        TRANSACTION_REF_NUMBER,
+        RESERVATION_REF_NUMBER,
+        PASSENGER_REF_NUMBER,
+        ASSIGNED_SEAT,
+        GOVT_AGENCY_REF_NUMBER,
+        CUSTOMER_REF_NUMBER
+    }
+    
+    private RffCode referenceCodeQualifier;
     private String referenceIdentifier;
     private int revisionIdentifier;
     
     public RFF(List<Composite> composites) {
         super(RFF.class.getSimpleName(), composites);
-        for (int i = 0; i < numComposites(); i++) {
-            Composite c = getComposite(i);
-            switch (i) {
-            case 0:
-                this.referenceCodeQualifier = c.getElement(0);
-                if (this.referenceCodeQualifier.equals("TN")) {
-                    this.referenceIdentifier = c.getElement(1);
-                    if (c.numElements() >= 5) {
-                        this.revisionIdentifier = Integer.valueOf(c.getElement(4));
-                    }
-                }
-            }
+        Composite c = getComposite(0);
+        if (c == null) {
+            return;
+        }
+         
+        String code = c.getElement(0);
+        if (StringUtils.isBlank(code)) {
+            return;
+        }
+        
+        switch(code) {
+        case "TN":
+            this.referenceCodeQualifier = RffCode.TRANSACTION_REF_NUMBER;
+            break;
+        case "AVF":
+            this.referenceCodeQualifier = RffCode.RESERVATION_REF_NUMBER;
+            break;
+        case "ABO":
+            this.referenceCodeQualifier = RffCode.PASSENGER_REF_NUMBER;
+            break;
+        case "SEA":
+            this.referenceCodeQualifier = RffCode.ASSIGNED_SEAT;
+            break;
+        case "AEA":
+            this.referenceCodeQualifier = RffCode.GOVT_AGENCY_REF_NUMBER;
+            break;
+        case "CR":
+            this.referenceCodeQualifier = RffCode.CUSTOMER_REF_NUMBER;
+            break;
+        }
+            
+        this.referenceIdentifier = c.getElement(1);
+        if (c.numElements() >= 5) {
+            this.revisionIdentifier = Integer.valueOf(c.getElement(4));
         }
     }
 
-    public String getReferenceCodeQualifier() {
+    public RffCode getReferenceCodeQualifier() {
         return referenceCodeQualifier;
     }
 
