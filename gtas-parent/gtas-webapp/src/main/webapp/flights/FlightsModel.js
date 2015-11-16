2ad72dc64ce1fc04628ea6306629384eb4b9b15a
Venu Tammineni <venu.tammineni@gmail.com>
2015-11-16 14:41:11 -0500

    filter eta start days initialization fix

--- gtas-parent/gtas-webapp/src/main/webapp/flights/FlightsModel.js
+++ gtas-parent/gtas-webapp/src/main/webapp/flights/FlightsModel.js
@@ -18,7 +18,7 @@ app.service("flightsModel", [function() {
     this.direction = 'I';
     this.etaStart = startDate;
     this.etaEnd = endDate;
-    this.sort = defaultSort;    
+    this.sort = defaultSort;
   }
   
   this.reset();
