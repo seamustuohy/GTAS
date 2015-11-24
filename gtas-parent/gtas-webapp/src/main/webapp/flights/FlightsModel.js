46ea8aa22e69aa5e8b158ba7196e0b7744f0a5d6
Anthony Delorie <de.lorie@gmail.com>
2015-11-24 11:40:57 -0500

    Updated Flights to allow autocomplete on the UI.. work in progress will load airports via JSON in next iteration

--- gtas-parent/gtas-webapp/src/main/webapp/flights/FlightsModel.js
+++ gtas-parent/gtas-webapp/src/main/webapp/flights/FlightsModel.js
@@ -1,25 +1,26 @@
-app.service("flightsModel", [function() {
-  var defaultSort = [
-      {column: 'ruleHitCount', dir: 'desc'},
-      {column: 'listHitCount', dir: 'desc'},
-      {column: 'eta', dir: 'desc'}
-    ],
-    startDate = new Date(),
-    endDate = new Date();
-    endDate.setDate(endDate.getDate() + 3);
-   
+app.service("flightsModel", [function () {
+    'use strict';
+    var defaultSort = [
+            {column: 'ruleHitCount', dir: 'desc'},
+            {column: 'listHitCount', dir: 'desc'},
+            {column: 'eta', dir: 'desc'}
+        ],
+        startDate = new Date(),
+        endDate = new Date();
 
-  this.reset = function() {
-    this.pageNumber = 1;
-    this.pageSize = 10;
-    this.flightNumber = '';
-    this.origin = '';
-    this.dest = '';
-    this.direction = 'I';
-    this.etaStart = startDate;
-    this.etaEnd = endDate;
-    this.sort = defaultSort;
-  }
-  
-  this.reset();
+    endDate.setDate(endDate.getDate() + 3);
+
+    this.reset = function () {
+        this.pageNumber = 1;
+        this.pageSize = 10;
+        this.flightNumber = '';
+        this.origin = '';
+        this.dest = '';
+        this.direction = 'I';
+        this.etaStart = startDate;
+        this.etaEnd = endDate;
+        this.sort = defaultSort;
+    };
+
+    this.reset();
 }]);
