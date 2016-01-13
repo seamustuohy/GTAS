3230eca543ba296d1f750f3cebdbbe844450d541
Amit <amit_bandopadhay@yahoo.com>
2016-01-13 10:18:04 -0500

    fixed merge issue

--- gtas-parent/gtas-commons/src/main/java/gov/gtas/enumtype/Status.java
+++ gtas-parent/gtas-commons/src/main/java/gov/gtas/enumtype/Status.java
@@ -2,11 +2,6 @@ package gov.gtas.enumtype;
 
 public enum Status {
 	SUCCESS,
-<<<<<<< Updated upstream
 	SUCCESS_WITH_WARNING,
 	FAILURE
-=======
-	FAILURE,
-	SUCCESS_WITH_WARNING
->>>>>>> Stashed changes
 }
